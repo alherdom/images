d67900a9f5a4399d9d6bcf95739452e35925e4a1
Alejandro Hdez <alherdom@outlook.com>
2023-10-23 17:01:47 +0100

    Complete Images app

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
 document.getElementById("data").addEventListener("submit", function (event) {
     event.preventDefault();
 
+    const table = document.getElementById('imageList');
+    table.innerHTML = '';
+
     var dh = dw = document.getElementById("size").value;
-    var bw = document.getElementById("border-width").value;
-    var bc = document.getElementById("border-color").value;
-    var sharpen = document.getElementById("image-focus").value;
-    var blur = document.getElementById("image-blur").value;
 
-    console.log("Size: " + dh);
-    console.log("Border Width: " + bw);
-    console.log("Border Color: " + bc);
-    console.log("Image Focus: " + sharpen);
-    console.log("Image Blur: " + blur);
+    if (dh > 1) {
+
+        var bh = bw = document.getElementById("border-width").value;
+        var bc = document.getElementById("border-color").value;
+        var bc = bc.slice(1, 7);
+        var sharpen = document.getElementById("image-focus").value;
+        var blur = document.getElementById("image-blur").value;
+
+        console.log("Size: " + dh);
+        console.log("Border Width: " + bw);
+        console.log("Border Color: " + bc);
+        console.log("Image Focus: " + sharpen);
+        console.log("Image Blur: " + blur);
+
+        const numImages = 20;
+        const URL_base = "http://10.109.18.40:80/img/"
+        for (let i = 1; i <= numImages; i++) {
+            const numberWithZero = i.toString().padStart(2, '0');
+            const imageURL = `${URL_base}image${numberWithZero}.jpg?dw=${dw}&dh=${dh}&bw=${bw}&bh=${bh}&bc=${bc}&sharpen=${sharpen}&blur=${blur}`;
+            const listItem = document.createElement("li");
+            const image = document.createElement("img");
+            image.src = imageURL;
+            listItem.appendChild(image);
+            imageList.appendChild(listItem);
+        }
+    }
+    else {
+        alert("The image size must be greather tan 1px");
+    }
 });
-
-const numImages = 20;
-for (let i = 1; i <= numImages; i++) {
-    const numberWithZero = i.toString().padStart(2, '0');
-    const imageURL = `http://$host:$port/small_light(${dw},${dh},${bw},${bc},${sharpen},${blur})/img/image.jpg${numberWithZero}.jpg`;
-    const listItem = document.createElement("li");
-    const image = document.createElement("img");
-    image.src = imageURL;
-    listItem.appendChild(image);
-    imageList.appendChild(listItem);
-}
