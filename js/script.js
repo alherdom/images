0c9040feb1ca7c38d4cdc3c8cd1a6936ede2fa89
Alejandro Hdez <alherdom@outlook.com>
2023-10-23 15:02:37 +0100

    Edit the js html

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 document.getElementById("data").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    var size = document.getElementById("size").value;
-    var border_width = document.getElementById("border-width").value;
-    var border_color = document.getElementById("border-color").value;
-    var image_focus = document.getElementById("image-focus").value;
-    var image_blur = document.getElementById("image-blur").value;
+    var dh = dw = document.getElementById("size").value;
+    var bw = document.getElementById("border-width").value;
+    var bc = document.getElementById("border-color").value;
+    var sharpen = document.getElementById("image-focus").value;
+    var blur = document.getElementById("image-blur").value;
 
-    console.log("Size: " + size);
-    console.log("Border Width: " + border_width);
-    console.log("Border Color: " + border_color);
-    console.log("Image Focus: " + image_focus);
-    console.log("Image Blur: " + image_blur);
+    console.log("Size: " + dh);
+    console.log("Border Width: " + bw);
+    console.log("Border Color: " + bc);
+    console.log("Image Focus: " + sharpen);
+    console.log("Image Blur: " + blur);
 });
 
 const numImages = 20;
 for (let i = 1; i <= numImages; i++) {
     const numberWithZero = i.toString().padStart(2, '0');
-    const imageURL = `img/image${numberWithZero}.jpg`;
+    const imageURL = `http://$host:$port/small_light(${dw},${dh},${bw},${bc},${sharpen},${blur})/img/image.jpg${numberWithZero}.jpg`;
     const listItem = document.createElement("li");
     const image = document.createElement("img");
     image.src = imageURL;
